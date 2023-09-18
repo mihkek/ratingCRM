b23d8208ede2bf4622e7dd0bf07309a66ff2fec2
mihkek <mihkek991@gmail.com>
2023-09-18 22:37:39 +0500

    update modible header

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -3,11 +3,14 @@
     <div id="dropdown" />
     <div class="md:flex md:flex-col">
       <div class="md:flex md:flex-col md:h-screen">
-        <div class="md:flex md:flex-shrink-0">
+        <div class="flex md:flex-shrink-0">
           <div class="flex items-center justify-between px-6 py-4 bg-white md:flex-shrink-0 md:justify-center md:w-56">
-            <Link class="mt-1" href="/">
-              <logo class="fill-white" width="120" height="28" />
-            </Link>
+            <div class="flex" href="/">
+              <BurderButton class="block md:hidden" @click="burderOpened = !burderOpened" />
+              <Link href="/">
+                <logo class="fill-white" width="120" height="28" />
+              </Link>
+            </div>
             <dropdown class="md:hidden" placement="bottom-end">
               <template #default>
                 <svg class="w-6 h-6 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
@@ -21,7 +24,7 @@
           </div>
           <div class="md:text-md flex items-center justify-between p-4 w-full text-sm bg-white border-b md:px-12 md:py-0">
 <!--            <div class="mr-4 mt-1">{{ auth.user.account.name }}</div>-->
-          <BurderButton class="block md:hidden" @click="burderOpened = !burderOpened" />
+
             <div class="mr-4 mt-1">
             </div>
             <dropdown class="mt-1" placement="bottom-end">
@@ -46,7 +49,7 @@
         </div>
         <div class="md:flex md:flex-grow md:overflow-hidden">
           <main-menu class="hidden flex-shrink-0 p-4 w-56 bg-indigo-800 overflow-y-auto md:block" />
-          <main-menu v-if="burderOpened" class="block flex-shrink-0 p-4 w-56 bg-indigo-800 overflow-y-auto md:hidden fixed top-119 z-50 h-full" />
+          <main-menu @click="burderOpened = false" v-if="burderOpened" class="block flex-shrink-0 p-4 w-56 bg-indigo-800 overflow-y-auto md:hidden fixed top-119 z-50 h-full" />
           <div class="px-4 py-8 md:flex-1 md:p-12 md:overflow-y-auto" scroll-region>
             <flash-messages />
             <slot />
